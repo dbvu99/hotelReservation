6de5affe9063fe433a3914e0182501b8d90d4365
dbvu99 <duc@bluespacecreative.com>
2022-07-09 17:29:38 -0500

    added FreeRoom and Room classes

---
+++ src/model/Room.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Room implements  IRoom {
+    public String roomNumber;
+    public Double price;
+    public RoomType enumeration;
+
+    public Room(String roomNumber, Double price, RoomType enumeration) {
+        this.roomNumber = roomNumber;
+        this.price = price;
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return price;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return enumeration;
+    }
+
+    @Override
+    public boolean isFree() {
+        return price == 0.0;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
